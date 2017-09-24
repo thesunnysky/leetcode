6367ff0ca2e204f87c5f576db3935417267c78d6
thesunnysky <jianyungsun@gmail.com>
2017-09-24 22:46:08 +0800

    comit

--- src/79_WrodSearch/WordSearch.go
+++ src/79_WrodSearch/WordSearch.go
@@ -1,41 +1,75 @@
 package _9_WrodSearch
 
-func wordSearch(board [][]rune, word string) bool {
+func exist(board [][]byte, word string) bool {
 	if len(board[0]) == 0 {
 		return false
 	}
 	for i := 0; i < len(board); i ++ {
 		for j := 0; j < len(board[0]); j++ {
-			if rune(word[0]) == board[i][j] && findWord(board, i, j, word, 1) {
-				return true
+			if rune(word[0]) == rune(board[i][j]) {
+				board[i][j] = '_'
+				if findWord(board, i, j, word, 1) {
+					board[i][j] = byte(word[0])
+					return true
+				}
+				board[i][j] = byte(word[0])
 			}
 		}
 	}
 	return false
 }
 
-func findWord(board [][]rune, i int, j int, word string, index int) bool {
-	if index > len(word) {
+func findWords(board [][]byte, words []string) []string {
+	keymap := make(map[string]int)
+	result := make([]string, 0)
+	for _, v := range words {
+		if exist(board, v) {
+			keymap[v] = 1
+		}
+	}
+	for ret := range keymap {
+		result = append(result,ret)
+	}
+	return result
+}
+
+func findWord(board [][]byte, i int, j int, word string, index int) bool {
+	if index >= len(word) {
 		return true
 	}
 
 	rowLimit, colLimit := len(board), len(board[0])
-	if i-1 >= 0 && rune(word[index]) == board[i-1][j] {
+	if i-1 >= 0 && byte(word[index]) == board[i-1][j] {
+		board[i-1][j] = '_'
 		if findWord(board, i-1, j, word, index+1) {
+			board[i-1][j] = byte(word[index])
 			return true
 		}
-	} else if i+1 < rowLimit && rune(word[index]) == board[i+1][j] {
+		board[i-1][j] = byte(word[index])
+	}
+	if i+1 < rowLimit && byte(word[index]) == board[i+1][j] {
+		board[i+1][j] = '_'
 		if findWord(board, i+1, j, word, index+1) {
+			board[i+1][j] = byte(word[index])
 			return true
 		}
-	} else if j-1 >= 0 && rune(word[index]) == board[i][j-1] {
+		board[i+1][j] = byte(word[index])
+	}
+	if j-1 >= 0 && byte(word[index]) == board[i][j-1] {
+		board[i][j-1] = '_'
 		if findWord(board, i, j-1, word, index+1) {
+			board[i][j-1] = byte(word[index])
 			return true
 		}
-	} else if j+1 < colLimit && rune(word[index]) == board[i][j+1] {
+		board[i][j-1] = byte(word[index])
+	}
+	if j+1 < colLimit && byte(word[index]) == board[i][j+1] {
+		board[i][j+1] = '_'
 		if findWord(board, i, j+1, word, index+1) {
+			board[i][j+1] = byte(word[index])
 			return true
 		}
+		board[i][j+1] = byte(word[index])
 	}
 	return false
 }
--- src/79_WrodSearch/WordSearch_test.go
+++ src/79_WrodSearch/WordSearch_test.go
@@ -3,6 +3,8 @@ package _9_WrodSearch
 import (
 	"testing"
 	"fmt"
+	"bytes"
+	"encoding/gob"
 )
 
 var board = [][]rune{
@@ -11,12 +13,43 @@ var board = [][]rune{
 	{'A', 'D', 'E', 'E'},
 }
 
-var words = []string{"SEE", "ABCCED", "SEE", "ABCB"}
+func genBoard() [][]byte {
+	var board = [][]byte{
+		{'A', 'B', 'C', 'E'},
+		{'S', 'F', 'C', 'S'},
+		{'A', 'D', 'E', 'E'},
+	}
+	return board
+}
+
+var words = []string{"ABCCED", "SEE", "ABCB"}
 
 func TestWordSearch(t *testing.T) {
+	temp := genBoard()
 	for _, v := range words {
-		if wordSearch(board, v) {
+		fmt.Println(temp)
+		if exist(temp, v) {
 			fmt.Println(v, "in board")
 		}
 	}
+}
+
+func TestFindWords(t *testing.T) {
+	words := []string{"oath", "pea", "eat", "rain"}
+	board := [][]byte{
+		{'o', 'a', 'a', 'n'},
+		{'e', 't', 'a', 'e'},
+		{'i', 'h', 'k', 'r'},
+		{'i', 'f', 'l', 'v'},
+	}
+	result := findWords(board, words)
+	fmt.Println(result)
+}
+
+func deepCopy(dst, src interface{}) error {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(src); err != nil {
+		return err
+	}
+	return gob.NewDecoder(bytes.NewBuffer(buf.Bytes())).Decode(dst)
 }
